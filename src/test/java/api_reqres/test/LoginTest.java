2f8330a85a4e4e85aa0ef9c74168071fb669e72a
Veranika Alifer <veranika.alifer@gmail.com>
2025-07-03 16:52:14 +0200

    Refactor login test suite to extract common request method

--- src/test/java/api_reqres/test/LoginTest.java
+++ src/test/java/api_reqres/test/LoginTest.java
@@ -4,7 +4,6 @@ import api_reqres.config.BaseTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.asynchttpclient.Request;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,52 +12,37 @@ public class LoginTest extends BaseTest {
     private static final String MISSING_EMAIL_MESSAGE = "Missing email or username";
     private static final String MISSING_PASSWORD_MESSAGE = "Missing password";
     private static final String USER_NOT_FOUND_MESSAGE = "user not found";
+    private static final int BAD_REQUEST_CODE = 400;
 
 
     @Test(description = "TC001 - Verifies that a user can successfully log in with valid credentials and receive a token.")
     public void testSuccessfulLogin() {
-
         String body = """
                  { 
                 "email": "eve.holt@reqres.in",
                  "password": "cityslicka"
                   } """;
 
-        Response loginResponse = RestAssured
-                .given()
-                .header("X-Api-Key", "reqres-free-v1")
-                .contentType("application/json")
-                .body(body)
-                .when()
-                .post(URL_LOGIN)
-                .then()
-                .statusCode(200)
-                .extract().response();
+        Response response = sendLoginRequest(body);
 
-        String token = loginResponse.jsonPath().getString("token");
+        String token = response.jsonPath().getString("token");
         Assert.assertNotNull(token, "Token is null!");
         Assert.assertFalse(token.isEmpty(), "Token is empty!");
+        Assert.assertEquals(response.getStatusCode(), 200);
     }
 
     @Test(description = "TC002 - Validates that the API returns an error when the email field is missing from the request.")
-    public void tesMissingEmail() {
+    public void testMissingEmail() {
         String invalidBody = """
                 {"password": "cityslicka"}
                  """;
 
-        Response response = RestAssured
-                .given()
-                .header("X-Api-Key", "reqres-free-v1")
-                .contentType(ContentType.JSON)
-                .body(invalidBody)
-                .when()
-                .post(URL_LOGIN)
-                .then()
-                .statusCode(400)
-                .extract().response();
+        Response response = sendLoginRequest(invalidBody);
 
         String error = response.jsonPath().getString("error");
         Assert.assertEquals(error, MISSING_EMAIL_MESSAGE, "Unexpected error message");
+        Assert.assertEquals(response.getStatusCode(), BAD_REQUEST_CODE);
+
     }
 
     @Test(description = "TC003 - Checks that the login fails if the password field is missing.")
@@ -66,19 +50,11 @@ public class LoginTest extends BaseTest {
         String invalidBody = """
                 {"email": "eve.holt@reqres.in"}""";
 
-        Response response = RestAssured
-                .given()
-                .header("X-Api-Key", "reqres-free-v1")
-                .contentType(ContentType.JSON)
-                .body(invalidBody)
-                .when()
-                .post(URL_LOGIN)
-                .then()
-                .statusCode(400)
-                .extract().response();
+        Response response = sendLoginRequest(invalidBody);
 
         String error = response.jsonPath().getString("error");
         Assert.assertEquals(error, MISSING_PASSWORD_MESSAGE, "Unexpected error message");
+        Assert.assertEquals(response.getStatusCode(), BAD_REQUEST_CODE);
     }
 
     @Test(description = "TC004 - Ensures login fails when using a non-existent or incorrect email.")
@@ -89,19 +65,12 @@ public class LoginTest extends BaseTest {
                 }
                 """;
 
-        Response response = RestAssured
-                .given()
-                .header("X-Api-Key", "reqres-free-v1")
-                .contentType(ContentType.JSON)
-                .body(invalidBody)
-                .when()
-                .post(URL_LOGIN)
-                .then()
-                .statusCode(400)
-                .extract().response();
+        Response response = sendLoginRequest(invalidBody);
 
         String error = response.jsonPath().getString("error");
         Assert.assertEquals(error, USER_NOT_FOUND_MESSAGE, "Unexpected error message");
+        Assert.assertEquals(response.getStatusCode(), BAD_REQUEST_CODE);
+
     }
 
     @Test(description = "TC005 — Verifies that login fails if the password is incorrect even if the email is valid.")
@@ -112,47 +81,47 @@ public class LoginTest extends BaseTest {
                 }
                 """;
 
-        Response response = RestAssured
-                .given()
-                .header("X-Api-Key", "reqres-free-v1")
-                .contentType(ContentType.JSON)
-                .body(invalidBody)
-                .when()
-                .post(URL_LOGIN)
-                .then().statusCode(400)
-                .extract().response();
+        Response response = sendLoginRequest(invalidBody);
+
+        String error = response.jsonPath().getString("error");
+        Assert.assertEquals(error, USER_NOT_FOUND_MESSAGE, "Unexpected error message");
+        Assert.assertEquals(response.getStatusCode(), BAD_REQUEST_CODE);
     }
 
-    @Test(description = "TC006 — ")
+    @Test(description = "TC006 — Verifies error response when the request body is empty.")
     public void testEmptyRequestBody() {
 
         String emptyBody = """
                 {}""";
 
-        Response response = RestAssured
-                .given()
-                .header("X-Api-Key", "reqres-free-v1")
-                .contentType(ContentType.JSON)
-                .body(emptyBody)
-                .when()
-                .post(URL_LOGIN)
-                .then()
-                .statusCode(400)
-                .extract().response();
+        Response response = sendLoginRequest(emptyBody);
 
         String error = response.jsonPath().getString("error");
         Assert.assertEquals(error, MISSING_EMAIL_MESSAGE);
+        Assert.assertEquals(response.getStatusCode(), BAD_REQUEST_CODE);
     }
 
-    @Test (description = "TC007 —  Validates that the endpoint does not allow methods other than POST (e.g., GET)")
-    public void testWrongHTTPMethod () {
+    @Test(description = "TC007 —  Validates that the endpoint does not allow methods other than POST (e.g., GET)")
+    public void testWrongHTTPMethod() {
         Response response = RestAssured
                 .given()
                 .header("X-Api-Key", "reqres-free-v1")
                 .contentType(ContentType.JSON)
                 .when()
                 .get(URL_LOGIN)
-                .then().statusCode(400)
+                .then().statusCode(BAD_REQUEST_CODE)
+                .extract().response();
+    }
+
+    private Response sendLoginRequest (String body) {
+        return  RestAssured
+                .given()
+                .header("X-Api-Key", "reqres-free-v1")
+                .contentType(ContentType.JSON)
+                .body(body)
+                .when()
+                .post(URL_LOGIN)
+                .then()
                 .extract().response();
     }
 }
